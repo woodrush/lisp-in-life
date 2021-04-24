5995172a236b5a9c41f29206a2f6ebce64e92fae
Hikaru Ikuta <woodrush924@gmail.com>
2021-04-24 09:10:22 +0900

    Mov folding

--- src/qftasmopt.py
+++ src/qftasmopt.py
@@ -60,77 +60,126 @@ n_registers = 11
 # reg_updated = {}
 # reg_referenced = {}
 
-reg_fresh = {}
 
 def readinst (inst):
     if len(inst) == 5:
         return tuple(inst) + ("",)
     return inst
 
-num_foldtimes = 0
-# Constant folding
+# # Constant folding
+# reg_fresh = {}
+# for i_inst, inst in enumerate(rom):
+#     lineno, opcode, (mode_1, d1), (mode_2, d2), (mode_3, d3), comment = readinst(inst)
+
+#     if mode_3 == 0 and d3 == 0 and not (mode_1 == 0 and d1 == 0 and opcode == "MNZ"):
+#         reg_fresh = {}
+#         # print("Detected jump")
+#         # print()
+
+#     if mode_1 > 0:
+#         if d1 in reg_fresh.keys() and reg_fresh[d1][1] == "const":
+#             # print("Constant folding is possible at line {}, with a value from {}".format(lineno, reg_fresh[d1]))
+#             rom[i_inst] = lineno, opcode, (mode_1 - 1, reg_fresh[d1][2]), (mode_2, d2), (mode_3, d3), comment
+
+#     if mode_2 > 0:
+#         if d2 in reg_fresh.keys() and reg_fresh[d2][1] == "const":
+#             # print("Constant folding is possible at line {}, with a value from {}".format(lineno, reg_fresh[d2]))
+#             rom[i_inst] = lineno, opcode, (mode_1, d1), (mode_2 - 1, reg_fresh[d2][2]), (mode_3, d3), comment
+#     if mode_3 > 0:
+#         if d3 in reg_fresh.keys() and reg_fresh[d3][1] == "const":
+#             # print("Constant folding is possible at line {}, with a value from {}".format(lineno, reg_fresh[d3]))
+#             rom[i_inst] = lineno, opcode, (mode_1, d1), (mode_2, d2), (mode_3 - 1, reg_fresh[d3][2]), comment
+
+#     if mode_3 == 0 and d3 != 0:
+#         if opcode == "MNZ" and mode_1 == 0 and mode_2 == 0 and not (d1 == 0):
+#             reg_fresh[d3] = lineno, "const", d2
+#         elif opcode == "MLZ" and mode_1 == 0 and mode_2 == 0 and not (type(d2) == int and (0 <= d2 < 32768)):
+#             reg_fresh[d3] = lineno, "const", d2
+#         elif opcode == "ADD" and mode_1 == 0 and mode_2 == 0:
+#             reg_fresh[d3] = lineno, "const", (d1 + d2)
+#         elif opcode == "SUB" and mode_1 == 0 and mode_2 == 0:
+#             reg_fresh[d3] = lineno, "const", (d1 - d2)
+#         else:
+#             reg_fresh[d3] = lineno, "uncertain", None
+
+#     if "pc ==" in comment:
+#         reg_fresh = {}
+    
+
+
+# Mov (register) folding
+reg_value = {}
+reg_value_dstedge = {}
+
+def add_dst_to_src(reg_dst, reg_src):
+    if reg_src not in reg_value_dstedge.keys():
+        reg_value_dstedge[reg_src] = []
+    reg_value_dstedge[reg_src].append(reg_dst)
+
+def remove_dependent_reference(reg):
+    if reg in reg_value_dstedge.keys():
+        for dst in reg_value_dstedge[reg]:
+            if dst in reg_value.keys():
+                del reg_value[dst]
+
 for i_inst, inst in enumerate(rom):
     lineno, opcode, (mode_1, d1), (mode_2, d2), (mode_3, d3), comment = readinst(inst)
 
     if mode_3 == 0 and d3 == 0 and not (mode_1 == 0 and d1 == 0 and opcode == "MNZ"):
-        reg_fresh = {}
+        reg_value = {}
+        reg_value_dstedge = {}
         # print("Detected jump")
         # print()
 
     if mode_1 > 0:
-        if d1 in reg_fresh.keys() and reg_fresh[d1][1] == "const":
-            # print("Constant folding is possible at line {}, with a value from {}".format(lineno, reg_fresh[d1]))
-            rom[i_inst] = lineno, opcode, (mode_1 - 1, reg_fresh[d1][2]), (mode_2, d2), (mode_3, d3), comment
-            num_foldtimes += 1
+        if d1 in reg_value.keys():
+            # print("Mov folding is possible at line {}, with a value from {}".format(lineno, reg_value[d1]))
+            _, (mode_table, dtable) = reg_value[d1]
+            rom[i_inst] = lineno, opcode, (mode_1 - 1 + mode_table, dtable), (mode_2, d2), (mode_3, d3), comment
 
     if mode_2 > 0:
-        if d2 in reg_fresh.keys() and reg_fresh[d2][1] == "const":
-            # print("Constant folding is possible at line {}, with a value from {}".format(lineno, reg_fresh[d2]))
-            rom[i_inst] = lineno, opcode, (mode_1, d1), (mode_2 - 1, reg_fresh[d2][2]), (mode_3, d3), comment
-            num_foldtimes += 1
+        if d2 in reg_value.keys():
+            # print("Mov folding is possible at line {}, with a value from {}".format(lineno, reg_value[d2]))
+            _, (mode_table, dtable) = reg_value[d2]
+            rom[i_inst] = lineno, opcode, (mode_1, d1), (mode_2 - 1 + mode_table, dtable), (mode_3, d3), comment
+
     if mode_3 > 0:
-        if d3 in reg_fresh.keys() and reg_fresh[d3][1] == "const":
-            # print("Constant folding is possible at line {}, with a value from {}".format(lineno, reg_fresh[d3]))
-            rom[i_inst] = lineno, opcode, (mode_1, d1), (mode_2, d2), (mode_3 - 1, reg_fresh[d3][2]), comment
-            num_foldtimes += 1
+        if d3 in reg_value.keys():
+            # print("Mov folding is possible at line {}, with a value from {}".format(lineno, reg_value[d3]))
+            _, (mode_table, dtable) = reg_value[d3]
+            rom[i_inst] = lineno, opcode, (mode_1, d1), (mode_2, d2), (mode_3 - 1 + mode_table, dtable), comment
+
 
     if mode_3 == 0 and d3 != 0:
-        if opcode == "MNZ" and mode_1 == 0 and mode_2 == 0 and not (d1 == 0):
-            reg_fresh[d3] = lineno, "const", d2
+        if opcode == "MNZ" and mode_1 == 0 and mode_2 <= 1 and not (d1 == 0):
+            reg_value[d3] = lineno, (mode_2, d2)
+            if mode_2 == 1:
+                add_dst_to_src(d3, d2)
         elif opcode == "MLZ" and mode_1 == 0 and mode_2 == 0 and not (type(d2) == int and (0 <= d2 < 32768)):
-            reg_fresh[d3] = lineno, "const", d2
+            reg_value[d3] = lineno, (mode_2, d2)
+            if mode_2 == 1:
+                add_dst_to_src(d3, d2)
         elif opcode == "ADD" and mode_1 == 0 and mode_2 == 0:
-            reg_fresh[d3] = lineno, "const", (d1 + d2)
+            reg_value[d3] = lineno, (mode_2, d1 + d2)
         elif opcode == "SUB" and mode_1 == 0 and mode_2 == 0:
-            reg_fresh[d3] = lineno, "const", (d1 - d2)
+            reg_value[d3] = lineno, (mode_2, d1 - d2)
         else:
-            reg_fresh[d3] = lineno, "uncertain", None
+            if d3 in reg_value.keys():
+                del reg_value[d3]
+        remove_dependent_reference(d3)
 
     if "pc ==" in comment:
-        reg_fresh = {}
+        reg_value = {}
+        reg_value_dstedge = {}
 
-    # print(lineno, reg_fresh)
-# 80 o
-# 800 x
-# 440 x
-# 260 x
-# 170 x
-# 125 o
-# 147 o
-# 158 x
-# 152 x
-# 150 x
-# 149 x
-# 148 x
 
-# 148
-    # if num_foldtimes > 148:
-    #     break
 
-reg_fresh = {}
+
 unused_lines = []
 
 # Dead code elimination
+reg_fresh = {}
+
 for inst in rom:
     lineno, opcode, (mode_1, d1), (mode_2, d2), (mode_3, d3), comment = readinst(inst)
 
@@ -154,10 +203,11 @@ for inst in rom:
             and reg_fresh[d3] != False
            ):  # `!= False`, since reg_fresh[d3] may be 0
             # print("Unused line: {} (overwriten at {})".format(reg_fresh[d3], lineno))
+
             unused_lines.append(reg_fresh[d3])
-            if "pc ==" in comment:
-                print("Error: Unused line with a pc tag!", lineno)
-                exit(1)
+            # if "pc ==" in comment:
+            #     print("Error: Unused line with a pc tag!", lineno)
+            #     exit(1)
 
         reg_fresh[d3] = lineno
 
--- src/run_optlisp.sh
+++ src/run_optlisp.sh
@@ -1,6 +1,8 @@
 python ./src/qftasmopt.py ../elvm/tmp.qftasmpp > opt.qftasmpp
-python ../elvm/tools/qftasm/qftasm_pp.py opt.qftasmpp > lisp_opt_v3.qftasm
-cat print.lisp | python ../elvm/tools/qftasm/qftasm_interpreter.py lisp_opt_v3.qftasm
+# python ./src/qftasmopt.py opt.qftasmpp > opt2.qftasmpp
+# python ./src/qftasmopt.py opt2.qftasmpp > opt3.qftasmpp
+python ../elvm/tools/qftasm/qftasm_pp.py opt.qftasmpp > lisp_opt.qftasm
+cat print.lisp | python ../elvm/tools/qftasm/qftasm_interpreter.py lisp_opt.qftasm
 
 # python qftasmopt.py ../elvm/tmp.qftasmpp > opt.qftasmpp
 # python qftasmopt.py opt.qftasmpp > opt2.qftasmpp
