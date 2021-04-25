d480a4e59662bd32fa6b4093b16f118385a2fe25
Hikaru Ikuta <woodrush924@gmail.com>
2021-04-25 16:51:29 +0900

    Add the optimized lisp source

--- src/elvm.h
+++ src/elvm.h
@@ -3,7 +3,7 @@ void placeholder (){
     const char* opstr = "define\0if\0quote\0car\0cdr\0cons\0atom\0print\0progn\0while\0lambda\0macro\0eval\0eq\0+\0-\0*\0/\0mod\0<\0>\0t\0";
 }
 
-#define QFTASM_RAMSTDIN_BUF_STARTPOSITION 1992
+#define QFTASM_RAMSTDIN_BUF_STARTPOSITION 1692
 #define QFTASM_MEM_OFFSET 0
 // #define QFTASM_MEM_OFFSET 1024
 // #define QFTASM_MEM_OFFSET 170
