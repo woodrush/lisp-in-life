ec7bb5a0ae6f3e6801e2f73c165973fbad4ea4b7
Hikaru Ikuta <woodrush924@gmail.com>
2021-04-27 18:00:26 +0900

    A slightly more memory-efficient objects2.lisp

--- objects2.lisp
+++ objects2.lisp
@@ -1,50 +1,38 @@
+(define defmethod (macro (methodname arglist body)
+  (list (quote define) (quote methodlist)
+        (list (quote cons)
+              (list (quote list)
+                    (list (quote quote) methodname)
+                    (list (quote lambda) arglist body))
+              (quote methodlist)))))
+(define _selector (lambda (methodlist targetmethod)
+  ((lambda (methodlist targetmethod curitem curname ret)
+    (progn
+      (while methodlist
+        (progn
+          (define curitem (car methodlist))
+          (define curname (car curitem))
+          (if (eq curname targetmethod)
+            (progn
+              (define methodlist ())
+              (define ret (car (cdr curitem))))
+            ())
+          (define methodlist (cdr methodlist))))
+      ret)
+  ) methodlist targetmethod)))
 (define counter (lambda ()
   ((closure (methodlist)
-  
     (progn
       ((closure (n)
         (progn
-          (define methodlist
-            (cons
-              (list (quote inc)
-                (lambda () (define n (+ n 1))))
-              methodlist))
-          (define methodlist
-            (cons
-              (list (quote get)
-                (lambda () n))
-              methodlist))
-          (define methodlist
-            (cons
-              (list (quote set)
-                (lambda (m) (define n m)))
-              methodlist))
-          
-          
-        )) ())
-
-      (lambda (callname)
-      
-        ((lambda (methodlist curitem curname ret)
-          (progn
-            (while methodlist
-              (progn
-                (define curitem (car methodlist))
-                (define curname (car curitem))
-                (if (eq curname callname)
-                  (progn
-                    (define methodlist ())
-                    (define ret (car (cdr curitem))))
-                  ())
-                (define methodlist (cdr methodlist))
-                  ))
-            ret)
-        ) methodlist)
-      )
-
-      )
-  ) () ) 
-))
+          (defmethod inc ()
+            (define n (+ n 1)))
+          (defmethod get ()
+            n)
+          (defmethod set (m)
+            (define n m))          
+        )))
+        (lambda (targetmethod)(_selector methodlist targetmethod)))))))
 
 (define counter1 (counter))
 (define counter2 (counter))
