5fae95b9b5a02717aac7fe5dc1c2c0e601340407
Hikaru Ikuta <woodrush924@gmail.com>
2021-04-28 20:47:53 +0900

    WIP: Optimize the string search tree

--- stringpos.py
+++ stringpos.py
@@ -1,4 +1,43 @@
-opstr = "eval atom quote macro lambda* define while progn lambda > < + - * / t mod print cons cdr car list eq if "
+from graphviz import Digraph
+
+opstr = "eval atom quote macro lambda* define while progn lambda > < + - * / t mod print cons cdr car list eq if"
+ops = opstr.split(" ")
+print(sorted(ops))
+
+def divide(l):
+    if len(l) == 0:
+        return None
+    # if len(l) == 1:
+    #     return {
+    #         "mid": l[0],
+    #         "lesser": None,
+    #         "greater": None
+    #     }
+    mid = len(l) // 2
+    return {
+        "mid": l[mid],
+        "lesser": divide(l[:mid]),
+        "greater": divide(l[mid+1:])
+    }
+tree = divide(ops)
+
+dg = Digraph(format="png")
+
+def setgraph(tree, dg):
+    dg.node(tree["mid"])
+    if tree["lesser"] is not None:
+        dg.edge(tree["mid"], tree["lesser"]["mid"])
+        setgraph(tree["lesser"], dg)
+    if tree["greater"] is not None:
+        dg.edge(tree["mid"], tree["greater"]["mid"])
+        setgraph(tree["greater"], dg)
+
+setgraph(tree, dg)
+
+dg.render("opgraph")
+
+exit()
+
 offset = 11
 poslist = [0+offset]
 for i_c, c in enumerate(opstr):
