5eeaaccdc429634dcfeda144169576b23f9d5594
Hikaru Ikuta <woodrush924@gmail.com>
2021-04-28 23:07:24 +0900

    Add graph without optimization

--- stringpos.py
+++ stringpos.py
@@ -1,8 +1,57 @@
 from graphviz import Digraph
 
 opstr = "eval atom quote macro lambda* define while progn lambda > < + - * / t mod print cons cdr car list eq if"
+
+
+def addtree(op, tree=None):
+    print(op, tree)
+    if tree is None:
+        return {
+            "mid": op,
+            "greater": None,
+            "lesser": None
+        }
+    elif op < tree["mid"]:
+        return {
+            "mid": tree["mid"],
+            "greater": tree["greater"],
+            "lesser": (
+                addtree(op, tree["lesser"])
+            ),
+        }
+    else:
+        return {
+            "mid": tree["mid"],
+            "greater": (
+                addtree(op, tree["greater"])
+            ),
+            "lesser": tree["lesser"],
+        }
+
+tree = None
+for op in opstr.split(" "):
+    tree = addtree(op, tree)
+
+def setgraph(tree, dg):
+    dg.node(tree["mid"])
+    if tree["lesser"] is not None:
+        dg.edge(tree["mid"], tree["lesser"]["mid"])
+        setgraph(tree["lesser"], dg)
+    if tree["greater"] is not None:
+        dg.edge(tree["mid"], tree["greater"]["mid"])
+        setgraph(tree["greater"], dg)
+
+        
+dg = Digraph(format="png")
+setgraph(tree, dg)
+dg.render("opgraph_v1")
+
+
+# opstr = "- cons while list t > macro if car print / + lambda define atom eq cdr mod * < progn lambda* quote eval"
 ops = opstr.split(" ")
-print(sorted(ops))
+ops = sorted(ops)
+# print(sorted(ops))
+
 
 def divide(l):
     if len(l) == 0:
@@ -21,22 +70,25 @@ def divide(l):
     }
 tree = divide(ops)
 
+
+def breadthfirst_print(tree):
+    queue = []
+    queue.append(tree)
+    while len(queue) > 0:
+        nextqueue = []
+        for item in queue:
+            print(item["mid"], end=" ")
+            if item["greater"] is not None:
+                nextqueue.append(item["greater"])
+            if item["lesser"] is not None:
+                nextqueue.append(item["lesser"])
+        queue = nextqueue
+breadthfirst_print(tree)
+
 dg = Digraph(format="png")
-
-def setgraph(tree, dg):
-    dg.node(tree["mid"])
-    if tree["lesser"] is not None:
-        dg.edge(tree["mid"], tree["lesser"]["mid"])
-        setgraph(tree["lesser"], dg)
-    if tree["greater"] is not None:
-        dg.edge(tree["mid"], tree["greater"]["mid"])
-        setgraph(tree["greater"], dg)
-
 setgraph(tree, dg)
-
 dg.render("opgraph")
 
-exit()
 
 offset = 11
 poslist = [0+offset]
